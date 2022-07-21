75132c34a8bba2cc5c157c3260dac86e7f64e65c
Sukma Suryaningsih <ingsih19@gmail.com>
2022-07-21 22:08:27 +0800

    add redirects

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
+// const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = '68683ee2503eaa161fc0c14530d2096d';
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 const tmdb = axios.create({
